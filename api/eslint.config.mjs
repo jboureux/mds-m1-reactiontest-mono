d4d4420c2c6f273446f4bbc26223a8728ba452fa
Jérémy Boureux <jeremyboureux@gmail.com>
2024-10-08 14:19:36 +0200

    feat: add eslint and prettier

---
+++ api/eslint.config.mjs
@@ -0,0 +1,35 @@
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import prettierConfig from "eslint-config-prettier";
+import globals from "globals";
+export default [
+	{
+		languageOptions: {
+			parser: typescriptParser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+			},
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+		plugins: {
+			"@typescript-eslint": typescriptPlugin,
+		},
+		rules: {
+			...typescriptPlugin.configs.recommended.rules,
+			...prettierConfig.rules, // Disabling rules that conflict with Prettier
+			"@typescript-eslint/no-unused-vars": [
+				"error",
+				{
+					argsIgnorePattern: "^_",
+					// Ignore variables prefixed with underscore
+				},
+			],
+			"@typescript-eslint/explicit-module-boundary-types": "off",
+			// Turn off explicit module boundary types
+		},
+	},
+];
