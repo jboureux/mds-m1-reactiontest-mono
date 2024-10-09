b337512683a816e2c3ba59e6c33e5b03c831986d
massDigit <massDigit@users.noreply.github.com>
2024-10-09 14:58:29 +0000

    prettier: Apply formatting changes

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,7 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-import usersRouter from './controllers/auth.controller';
+import usersRouter from "./controllers/auth.controller";
 import { TimerRouter } from "./controllers/timer.controller";
 
 dotenv.config();
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use('/users', usersRouter);
+app.use("/users", usersRouter);
 app.use("/timer", TimerRouter);
 
 const port = 1234;
--- api/tests/services/authentification/auth.login.service.test.ts
+++ api/tests/services/authentification/auth.login.service.test.ts
@@ -1,38 +1,51 @@
-
 import { loginUser } from "../../../src/services/auth.services";
 import User, { IUser } from "../../../src/models/user.models";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-import {jest , describe ,it ,expect}from "@jest/globals"
-
+import { jest, describe, it, expect } from "@jest/globals";
 
 jest.mock("../../../src/models/user.models");
 jest.mock("bcryptjs");
 jest.mock("jsonwebtoken");
 
 describe("loginUser", () => {
-  it("devrait renvoyer une erreur si l'utilisateur n'est pas trouvé", async () => {
-   
-    (User.findOne as jest.MockedFunction<typeof User.findOne>).mockResolvedValue(null);
-    await expect(loginUser("test@test.com", "password123")).rejects.toThrow("Invalid credentials");
-  });
+    it("devrait renvoyer une erreur si l'utilisateur n'est pas trouvé", async () => {
+        (
+            User.findOne as jest.MockedFunction<typeof User.findOne>
+        ).mockResolvedValue(null);
+        await expect(loginUser("test@test.com", "password123")).rejects.toThrow(
+            "Invalid credentials",
+        );
+    });
 
-  it("devrait renvoyer une erreur si le mot de passe est incorrect", async () => {
-    const mockUser = { password: bcrypt.hashSync("wrongpassword", 10) } as IUser;
-    (User.findOne as jest.MockedFunction<typeof User.findOne>).mockResolvedValue(mockUser); 
-    (bcrypt.compareSync as jest.Mock).mockReturnValue(false); 
+    it("devrait renvoyer une erreur si le mot de passe est incorrect", async () => {
+        const mockUser = {
+            password: bcrypt.hashSync("wrongpassword", 10),
+        } as IUser;
+        (
+            User.findOne as jest.MockedFunction<typeof User.findOne>
+        ).mockResolvedValue(mockUser);
+        (bcrypt.compareSync as jest.Mock).mockReturnValue(false);
 
-    await expect(loginUser("test@test.com", "password123")).rejects.toThrow("Invalid credentials");
-  });
+        await expect(loginUser("test@test.com", "password123")).rejects.toThrow(
+            "Invalid credentials",
+        );
+    });
 
-  it("devrait renvoyer un token si les identifiants sont corrects", async () => {
-    const mockUser = { id: "1", email: "test@test.com", password: bcrypt.hashSync("password123", 10) } as IUser;
-    (User.findOne as jest.MockedFunction<typeof User.findOne>).mockResolvedValue(mockUser);
-    (bcrypt.compareSync as jest.Mock).mockReturnValue(true); 
-    (jwt.sign as jest.Mock).mockReturnValue("mockedToken"); 
+    it("devrait renvoyer un token si les identifiants sont corrects", async () => {
+        const mockUser = {
+            id: "1",
+            email: "test@test.com",
+            password: bcrypt.hashSync("password123", 10),
+        } as IUser;
+        (
+            User.findOne as jest.MockedFunction<typeof User.findOne>
+        ).mockResolvedValue(mockUser);
+        (bcrypt.compareSync as jest.Mock).mockReturnValue(true);
+        (jwt.sign as jest.Mock).mockReturnValue("mockedToken");
 
-    const result = await loginUser("test@test.com", "password123");
-    expect(result).toHaveProperty("token", "mockedToken");
-  });
+        const result = await loginUser("test@test.com", "password123");
+        expect(result).toHaveProperty("token", "mockedToken");
+    });
 });
