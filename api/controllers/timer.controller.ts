c55dee9c31329e5c41c8dc982a5d45148f13529e
Jérémy Boureux <jeremyboureux@gmail.com>
2024-10-09 14:09:06 +0200

    feat: add Controller

---
+++ api/controllers/timer.controller.ts
@@ -0,0 +1,59 @@
+import express from "express";
+import mongoose from "mongoose";
+import {
+    clearUserTimers,
+    getUserTimers,
+    uploadTimer,
+} from "../src/services/timer.service";
+
+const router = express.Router();
+
+router.post("/submit", async (req, res) => {
+    const { reactionTime } = req.body;
+
+    if (reactionTime === undefined) {
+        res.status(400).send({
+            status: 400,
+            error: "You should provide a 'reactionTime' in the request body",
+        });
+        return;
+    }
+
+    //check authentication and get userId
+
+    try {
+        const timer = await uploadTimer(
+            reactionTime,
+            new mongoose.Types.ObjectId("67066e20c0db3a6728fa99c6"),
+        );
+        res.status(201).send(timer);
+    } catch (e) {
+        res.status(400).send({ status: 400, error: (e as Error).message });
+    }
+});
+router.get("/me", async (req, res) => {
+    //check auth and get UserID
+    try {
+        const timers = await getUserTimers(
+            new mongoose.Types.ObjectId("67066e20c0db3a6728fa99c6"),
+        );
+
+        res.status(200).send(timers);
+    } catch (e) {
+        res.status(400).send({ status: 400, error: (e as Error).message });
+    }
+});
+router.delete("/clear", async (req, res) => {
+    //check auth and get UserID
+    try {
+        const timers = await clearUserTimers(
+            new mongoose.Types.ObjectId("67066e20c0db3a6728fa99c6"),
+        );
+
+        res.status(200).send(timers);
+    } catch (e) {
+        res.status(400).send({ status: 400, error: (e as Error).message });
+    }
+});
+
+export const TimerRouter = router;
