4c74c4f552ffb60ea93e4493efeb1e9bffd5a9c9
massDigit <massDigit@users.noreply.github.com>
2024-10-09 20:24:56 +0000

    prettier: Apply formatting changes

--- api/tests/services/authentification/auth.login.service.test.ts
+++ api/tests/services/authentification/auth.login.service.test.ts
@@ -1,58 +1,65 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { describe, it, expect, beforeAll, afterAll, afterEach } from "@jest/globals";
+import {
+    describe,
+    it,
+    expect,
+    beforeAll,
+    afterAll,
+    afterEach,
+} from "@jest/globals";
 import { loginUser } from "../../../src/services/auth.service";
 import User from "../../../src/models/user.models";
 
 beforeAll(async () => {
-  await mongoose.connect(`${process.env.DATABASE_URL}_test_users`, {});
+    await mongoose.connect(`${process.env.DATABASE_URL}_test_users`, {});
 });
 
 afterEach(async () => {
-  await User.deleteMany({});
+    await User.deleteMany({});
 });
 
 afterAll(async () => {
-  await mongoose.connection.db?.dropDatabase();
-  await mongoose.connection.close();
+    await mongoose.connection.db?.dropDatabase();
+    await mongoose.connection.close();
 });
 
 describe("loginUser tests", () => {
-  it("devrait renvoyer une erreur si l'utilisateur n'est pas trouvé", async () => {
-   
-    await expect(loginUser("test@test.com", "password123")).rejects.toThrow(
-      "Invalid credentials"
-    );
-  });
-
-  it("devrait renvoyer une erreur si le mot de passe est incorrect", async () => {
-   
-    const existingUser = new User({
-      username: "testuser",
-      email: "test@test.com",
-      password: bcrypt.hashSync("correctpassword", 10),
+    it("devrait renvoyer une erreur si l'utilisateur n'est pas trouvé", async () => {
+        await expect(loginUser("test@test.com", "password123")).rejects.toThrow(
+            "Invalid credentials",
+        );
     });
-    await existingUser.save();
 
-    await expect(loginUser("test@test.com", "wrongpassword")).rejects.toThrow(
-      "Invalid credentials"
-    );
-  });
+    it("devrait renvoyer une erreur si le mot de passe est incorrect", async () => {
+        const existingUser = new User({
+            username: "testuser",
+            email: "test@test.com",
+            password: bcrypt.hashSync("correctpassword", 10),
+        });
+        await existingUser.save();
 
-  it("devrait renvoyer un token si les identifiants sont corrects", async () => {
-
-    const existingUser = new User({
-      username: "testuser",
-      email: "test@test.com",
-      password: bcrypt.hashSync("password123", 10), 
+        await expect(
+            loginUser("test@test.com", "wrongpassword"),
+        ).rejects.toThrow("Invalid credentials");
     });
-    await existingUser.save();
 
-    const result = await loginUser("test@test.com", "password123");
+    it("devrait renvoyer un token si les identifiants sont corrects", async () => {
+        const existingUser = new User({
+            username: "testuser",
+            email: "test@test.com",
+            password: bcrypt.hashSync("password123", 10),
+        });
+        await existingUser.save();
 
-    expect(result).toHaveProperty("token");
-    const decoded = jwt.verify(result.token, process.env.JWT_SECRET || "secret");
-    expect(decoded).toHaveProperty("email", "test@test.com");
-  });
+        const result = await loginUser("test@test.com", "password123");
+
+        expect(result).toHaveProperty("token");
+        const decoded = jwt.verify(
+            result.token,
+            process.env.JWT_SECRET || "secret",
+        );
+        expect(decoded).toHaveProperty("email", "test@test.com");
+    });
 });
--- api/tests/services/authentification/auth.register.service.test.ts
+++ api/tests/services/authentification/auth.register.service.test.ts
@@ -1,74 +1,84 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { describe, it, expect, beforeAll, afterAll, afterEach } from "@jest/globals";
+import {
+    describe,
+    it,
+    expect,
+    beforeAll,
+    afterAll,
+    afterEach,
+} from "@jest/globals";
 import { registerUser } from "../../../src/services/auth.service";
 import User from "../../../src/models/user.models";
 import { log } from "console";
 
 // Connexion à la base de données
 beforeAll(async () => {
-  await mongoose.connect(`${process.env.DATABASE_URL}_test_users`, {
-   
-  });
+    await mongoose.connect(`${process.env.DATABASE_URL}_test_users`, {});
 });
 
 // Nettoyage après chaque test
 afterEach(async () => {
-  await User.deleteMany({});
+    await User.deleteMany({});
 });
 
 // Déconnexion après tous les tests
 afterAll(async () => {
-  await mongoose.connection.db?.dropDatabase();
-  await mongoose.connection.close();
+    await mongoose.connection.db?.dropDatabase();
+    await mongoose.connection.close();
 });
 
 function checkUserFields(expectedFields: Record<string, any>, actualUser: any) {
-  for (const field in expectedFields) {
-    expect(actualUser).toHaveProperty(field, expectedFields[field]);
-  }
+    for (const field in expectedFields) {
+        expect(actualUser).toHaveProperty(field, expectedFields[field]);
+    }
 }
 
 describe("registerUser tests", () => {
-  it("devrait renvoyer une erreur si l'utilisateur existe déjà", async () => {
-   
-    const existingUser = new User({
-      username: "testuser",
-      email: "test@test.com",
-      password: bcrypt.hashSync("password123", 10),
+    it("devrait renvoyer une erreur si l'utilisateur existe déjà", async () => {
+        const existingUser = new User({
+            username: "testuser",
+            email: "test@test.com",
+            password: bcrypt.hashSync("password123", 10),
+        });
+        await existingUser.save();
+
+        await expect(
+            registerUser("newuser", "test@test.com", "password123"),
+        ).rejects.toThrow("User already exists");
     });
-    await existingUser.save();
 
-    
-    await expect(
-      registerUser("newuser", "test@test.com", "password123")
-    ).rejects.toThrow("User already exists");
-  });
+    it("devrait créer un nouvel utilisateur et renvoyer un token", async () => {
+        const result = await registerUser(
+            "newuser",
+            "new@test.com",
+            "password123",
+        );
 
-  it("devrait créer un nouvel utilisateur et renvoyer un token", async () => {
-    
-    const result = await registerUser("newuser", "new@test.com", "password123");
+        const savedUser = await User.findOne({ email: "new@test.com" });
+        expect(savedUser).toBeDefined();
+        expect(savedUser?.email).toBe("new@test.com");
 
-    
-    const savedUser = await User.findOne({ email: "new@test.com" });
-    expect(savedUser).toBeDefined();
-    expect(savedUser?.email).toBe("new@test.com");
+        const isPasswordValid = bcrypt.compareSync(
+            "password123",
+            savedUser?.password!,
+        );
+        expect(isPasswordValid).toBe(true);
 
-    const isPasswordValid = bcrypt.compareSync("password123", savedUser?.password!);
-    expect(isPasswordValid).toBe(true);
+        const expectedFields = {
+            email: "new@test.com",
+            username: "newuser",
+            password: savedUser?.password,
+        };
 
-    const expectedFields = {
-      email: "new@test.com",
-      username: "newuser",
-      password: savedUser?.password,
-    };
+        checkUserFields(expectedFields, savedUser);
 
-    checkUserFields(expectedFields, savedUser);
-
-    
-    expect(result).toHaveProperty("token");
-    const decoded = jwt.verify(result.token, process.env.JWT_SECRET || "secret");
-    expect(decoded).toHaveProperty("email", "new@test.com");
-  });
+        expect(result).toHaveProperty("token");
+        const decoded = jwt.verify(
+            result.token,
+            process.env.JWT_SECRET || "secret",
+        );
+        expect(decoded).toHaveProperty("email", "new@test.com");
+    });
 });
