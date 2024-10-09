7f4100cd286b523b4c1a6d559abbcffbd7d8ec6e
Jérémy Boureux <jeremyboureux@gmail.com>
2024-10-09 16:00:53 +0200

    [8] Creation service timer + Controller (#15)
    * feat: add timer service and tests scenraios (not implemented)
    
    * add coverage folder to .gitignore
    
    * feat: add Controller
    
    * chore: edit tsconfig
    
    * chore: register TimerRouter
    
    * fix: model user type is an array
    
    * feat: fix timer service and declare tests
    
    * update des trucs
    
    * update CI
    
    * fix ci

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import { TimerRouter } from "../controllers/timer.controller";
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(cors());
 
 const port = 1234;
+
+app.use("/timer", TimerRouter);
 
 app.listen(port, async () => {
     await mongoose.connect(`${process.env.DATABASE_URL}`);
